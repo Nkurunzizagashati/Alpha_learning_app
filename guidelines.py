f9afabc6909d11be125682c9e25fefcb8d76bb47
Fab <f.nkuruniz@alustudent.com>
2022-11-03 07:52:27 -0700

    This function will give the user guidelines for the project which will help him to be able to run the game

---
+++ guidelines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import time
+
+def guid():
+    print("- Hello! thank you for your interest in learning alphabets and your welcome on Alpha_learning_app\n")
+    print("- This is a game that will help you to learn aphabets, the game have 3 levels: level 1 for beginners, level 2 for intermediate, and level 3 which is advanced level.\n")
+
+    time.sleep (1)
+    print("- At the end of each level you will be given a quiz and you must pass the quiz with at least 75% ti continue to the next level\n")
+
+    time.sleep (1)
+    print("- After completing the quiz with the required marks, you will be given an option continue to the next step or not. You will be asked a  question 'do you want to continue or not?' an you will type 1 to continue or 2 if you don't want to continue.\n")
+
+    print("- If you fail the quiz, you will have a chance to repeat again but you will start from the scratch.\n")
+
+    time.sleep (2)
+
+    print("\"All the best for your future\"")
